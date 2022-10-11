a7b99e9bc0159d10d814f8e9b3ece4897d46d626
niujinshuchong <yuzh@shanghaitech.edu.cn>
2022-10-11 08:25:51 +0200

    fix link

--- scripts/download_highres_TNT.sh
+++ scripts/download_highres_TNT.sh
@@ -1,6 +1,6 @@
 # TNT hihgres depth map and normal map
 cd data
 
-wget https://s3.eu-central-1.amazonaws.com/avg-projects/monosdf/data/hihgresTNT.tar
+wget https://s3.eu-central-1.amazonaws.com/avg-projects/monosdf/data/highresTNT.tar
 tar -xf hihgresTNT.tar
 rm -rf hihgresTNT.tar
